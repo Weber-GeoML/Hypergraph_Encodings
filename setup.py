b4e1bf54142ded9ab2a63e302399d032b7739313
Raphael <pellegrinraphael@Pellegrins-MacBook-Pro.local>
2024-06-26 18:05:12 -0400

    adding dev requires to setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_namespace_packages, setup
 
 setup(
-    name='encodings',
-    version='1.0.0',
-    python_requires=">3.10",
-    packages=find_namespace_packages(where='src'),
+    name="encodings",
+    version="1.0.0",
+    python_requires=">3.11",
+    packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[
-        "numpy",
-        "pandas",
-    ],
-    extra_requires = {"dev"=[],"test"=[]},
+    install_requires=["numpy", "pandas", "matplotlib"],
+    extra_requires={
+        "dev": ["mypy", "pandas-stubs", "black", "isort"],
+        "test": ["pytest", "coverage"],
+    },
 )
