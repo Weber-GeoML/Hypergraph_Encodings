78b4138983ca5c396ba5fffe3add6adf09b9ad38
Raphael <pellegrinraphael@Pellegrins-MacBook-Pro.local>
2024-08-08 20:05:14 -0400

    checking that node degree does not depend on node name

--- tests/test_curvature.py
+++ tests/test_curvature.py
@@ -32,6 +32,27 @@ def toy_hypergraph() -> dict[str, dict]:
     return hg
 
 
+@pytest.fixture
+def toy_hypergraph_2() -> dict[str, dict]:
+    """Build toy hypergraph
+
+    Returns:
+        toy_hypergraph:
+            hypergraph from draft
+    """
+    # We don't care about features or labels
+    hg: dict[str, dict] = {
+        "hypergraph": {
+            "yellow": [4, 5, 7],
+            "red": [5, 7],
+        },
+        "features": {},
+        "labels": {},
+        "n": 3,
+    }
+    return hg
+
+
 def test_compute_node_degrees(toy_hypergraph) -> None:
     """
     Test for compute_node_degrees
@@ -45,6 +66,21 @@ def test_compute_node_degrees(toy_hypergraph) -> None:
     forman_ricci.compute_forman_ricci()
     print(forman_ricci.node_degrees)
     assert forman_ricci.node_degrees == {1: 1, 2: 2, 3: 3, 4: 1, 5: 2, 6: 1}
+
+
+def test_compute_node_degrees_2(toy_hypergraph_2) -> None:
+    """
+    Test for compute_node_degrees
+
+    Args:
+        toy_hypergraph_2:
+            hypergraph where the nodes do not start at 1
+    """
+    forman_ricci = FormanRicci(toy_hypergraph_2)
+    # Computes the Forman-Ricci curvature
+    forman_ricci.compute_forman_ricci()
+    print(forman_ricci.node_degrees)
+    assert forman_ricci.node_degrees == {4: 1, 5: 2, 7: 2}
 
 
 def test_compute_forman_ricci(toy_hypergraph) -> None:
