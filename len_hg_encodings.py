bbaa2af52f067b12631fe0ee7700e931d493e52c
Raphael <pellegrinraphael@Pellegrins-MacBook-Pro.local>
2024-11-13 00:41:22 -0500

    to check length

---
+++ len_hg_encodings.py
@@ -0,0 +1,25 @@
+import pickle
+import os
+
+# Directory where the pickle files are stored
+# directory = '/Users/pellegrinraphael/Desktop/Repos_GNN/Hypergraph_Encodings/data/hypergraph_classification_datasets'
+directory = "/Users/pellegrinraphael/Desktop/Repos_GNN/Hypergraph_Encodings/data/hypergraph_classification_datasets"
+# Iterate over all files in the directory
+
+# Get all pickle files and sort them alphabetically
+filenames = sorted([f for f in os.listdir(directory) if f.endswith(".pickle")])
+
+
+for filename in filenames:
+    if filename.endswith(".pickle"):
+        file_path = os.path.join(directory, filename)
+
+        # Load the pickle file
+        with open(file_path, "rb") as f:
+            data = pickle.load(f)
+
+            # Check if the loaded data is a list
+            if isinstance(data, list):
+                print(f"{filename}: {len(data)} elements")
+            else:
+                print(f"{filename}: Not a list")
