e4c88866095e47e4344d2fd256475259697f587c
Raphael <pellegrinraphael@Pellegrins-MacBook-Pro.local>
2025-01-23 23:09:19 -0500

    can toggle

--- src/brec_analysis/encodings_to_check.py
+++ src/brec_analysis/encodings_to_check.py
@@ -4,8 +4,8 @@ ENCODINGS_TO_CHECK = [
     # ("LDP", "Local Degree Profile"),
     # ("LCP-FRC", "Local Curvature Profile - FRC"),
     # ("RWPE", "Random Walk Encodings"),
-    # ("LCP-ORC", "Local Curvature Profile - ORC"),
-    ("LAPE-Normalized", "Normalized Laplacian"),
-    ("LAPE-RW", "Random Walk Laplacian"),
-    ("LAPE-Hodge", "Hodge Laplacian"),
+    ("LCP-ORC", "Local Curvature Profile - ORC"),
+    # ("LAPE-Normalized", "Normalized Laplacian"),
+    # ("LAPE-RW", "Random Walk Laplacian"),
+    # ("LAPE-Hodge", "Hodge Laplacian"),
 ]
